8b3e6c073322aa5713fb2b20bc7055357ee300d2
edn869728-jpg <edn869728@gmail.com>
2026-06-06 01:26:42 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// ANG HR GitHub Pages 前端設定
+// 只要換 GAS Web App 或 Cloudflare Worker 網址，改這裡就好。
+window.ANG_HR_CONFIG = {
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyLqYPsz-wcA7fW8KA1OylkJvmiIlwyEpSaKB0ovUxsomRSjKmHTRToK-C5Uf-i3BjSPg/exec',
+  employeePageUrl: './employee.html',
+  adminPageUrl: './admin.html',
+  defaultRole: 'Creator'
+};
