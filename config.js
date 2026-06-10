9489d5c32720f6b8216c3808e18ebd12e16e0c41
edn869728-jpg <edn869728@gmail.com>
2026-06-11 01:39:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ANG_HR_CONFIG = {
   appName: 'ANG HR System',
-  gasApiUrl: 'https://script.google.com/macros/s/AKfycbyLqYPsz-wcA7fW8KA1OylkJvmiIlwyEpSaKB0ovUxsomRSjKmHTRToK-C5Uf-i3BjSPg/exec',
+  gasApiUrl: 'https://script.google.com/macros/s/AKfycbyVD-_tx9wFkggsuSnrL2T6HoyGPYuBQgoClr4GyxISaAeNfLVY32auEZDw2ihrXqJj/exec',
   googleClientId: '466961332869-icjvb1i9b6q9ghiei0oslhffpcgp57v1.apps.googleusercontent.com',
   themeColors: ['#FF87E0', '#CCA4FF', '#8089FF', '#59DDFF'],
   defaultCompanyId: '',
