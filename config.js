c65ac453a1b839ab3ad3ee23d21adc0ce0a83875
edn869728-jpg <edn869728@gmail.com>
2026-06-11 01:13:13 +0800

    Update configuration values for GAS and Google OAuth
    Updated GAS API URL and Google OAuth Client ID.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ANG_HR_CONFIG = {
   appName: 'ANG HR System',
-  gasApiUrl: '請貼上你的 GAS Web App URL，例如：https://script.google.com/macros/s/部署ID/exec',
-  googleClientId: '請填入你的 Google OAuth Web Client ID',
+  gasApiUrl: 'https://script.google.com/macros/s/AKfycbyVD-_tx9wFkggsuSnrL2T6HoyGPYuBQgoClr4GyxISaAeNfLVY32auEZDw2ihrXqJj/exec',
+  googleClientId: '466961332869-icjvb1i9b6q9ghiei0oslhffpcgp57v1.apps.googleusercontent.com',
   themeColors: ['#FF87E0', '#CCA4FF', '#8089FF', '#59DDFF'],
   defaultCompanyId: '',
   defaultEmployeeId: ''
